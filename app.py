c8cd25bc8e3866a43bc2770e0de3a410edfa3506
keefm6776 <keefm_6776@yahoo.com>
2020-07-30 17:59:17 +0000

    rednder template in app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+@app.route('/')
+def show_portfolio():
+    return render_template("index.html")
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
