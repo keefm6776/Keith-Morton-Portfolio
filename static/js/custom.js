43cb1369ea546d23799b7f079034112aa3598a05
keefm6776 <keefm_6776@yahoo.com>
2020-07-31 19:55:23 +0000

    validate form input with javascript and email form information with formspree

---
+++ static/js/custom.js
@@ -0,0 +1,21 @@
+function validateForm() {
+  var x = document.forms["contact-form"]["name"].value;
+  var y = document.forms["contact-form"]["message"].value;
+  var email = document.forms["contact-form"]["email"].value;
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (x == "") {
+    alert("Name must be filled out!");
+    return false;
+  }
+  if (y == "") {
+    alert("You must include a message!");
+    return false;
+  }
+  if (re.test(email) == false) {
+    alert("Your email is invalid!");
+    return false;
+  }
+  
+} 
+
